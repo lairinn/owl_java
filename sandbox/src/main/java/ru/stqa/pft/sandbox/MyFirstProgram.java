4bf0b7a85a8970251fad6e5e7f535c1d6ab855ca
Julia <lairinna@gmail.com>
2018-05-07 17:09:02 +0300

    вычисление расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 public static void main (String[] args) {
-System.out.println("Hello, world!"); 
+
+  Point p1 = new Point();
+ p1.x = 1;
+ p1.y = 1;
+
+  Point p2 = new Point();
+  p2.x = 0;
+ p2.y = 0;
+
+  double result = p2.distance(p1);
+  System.out.println("Расстояние = " + result);
+
+
+
+    System.out.println("Расстояние = " + distance(p1,p2));
 }
 
+  public static double distance(Point p1, Point p2) {
+    double d;
+    d = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+    Math.sqrt(d);
+    d = Math.sqrt(d);
+    return d;
+  }
+
 }
+
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by ishulga on 07.05.2018.
+ */
+public class Point {
+
+  double x;
+  double y;
+
+  public double distance(Point p2) {
+    double d;
+    d = (this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y);
+    Math.sqrt(d);
+    d = Math.sqrt(d);
+    return d;
+  }
+
+
+
+}
+
+
+
+
+
